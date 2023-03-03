8f95fc7b6bf4f52855c55abafa6cf65ca02465db
Trent Osterman <trentosterman@gmail.com>
2023-03-03 16:16:36 -0600

    got my post to send info to my server and now i am making functions to run those values.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,10 +13,14 @@ function onReady () {
     // then it will run the function operator input.
     $('.operatorBtn').on('click', operatorInput)
 
+    // this is a listener for when i click '=' it will send the data to the server
+    // where it will then be computed
+    $('.equalBtn').on('click', sendInputs);
+
 
 }
 
-// this is a fucntion that will give me the value of the button i click on.
+//this is a fucntion that will give me the value of the button i click on.
 function numberInput () {
     pickedNumber = $(this).text()
     console.log(pickedNumber);
@@ -26,10 +30,49 @@ function numberInput () {
 function operatorInput () {
     pickedOperator = $(this).text()
     console.log(pickedOperator);
+
 }
 
-let firstInput = '';
-let secondInput = '';
-let operator = operatorInput();
+// these are the inputs i will need to append later on.
+let firstInput = $('#firstNumber').val();
+let secondInput = $('#secondNumber').val();
+let operator = '';
 let answer = '';
 let pastInputs = '';
+
+
+console.log(firstInput)
+console.log(secondInput)
+console.log(operator)
+
+
+function sendInputs () {
+    console.log('inside send inputs function.')
+
+    // this is delaring the variables that get put into the 
+    let firstInput = $('#firstNumber').val();
+    let secondInput = $('#secondNumber').val();
+    let operator = pickedOperator;
+
+
+
+    let allInputs = {
+        part1: firstInput,
+        operator1: operator,
+        part2: secondInput,
+    }
+    // this is a console.log() to see what the allinputs is
+    console.log(allInputs)
+
+    // now i will make the post ajax to send the inputs to the server
+    $.ajax({
+        method: 'POST',
+        url: '/calculator',
+        data: allInputs
+    }).then((response) => {
+        console.log('the post worked!')
+    }).catch((response) => {
+        alert('post did not work')
+    })
+}
+
--- server/server.js
+++ server/server.js
@@ -12,10 +12,30 @@ app.use(express.static('server/public'))
 
 
 // this is the variable that hosts our total result
-let totalResult = {
+let totalResult = [{
+    part1: '',
+    operator: '',
+    part2: '',
     total: 'test total',
     historical: ['1+2', '1+4'],
+}]
+
+// my add function
+function addInputs (num1, num2) {
+    let total = num1 + num2
+    return total;
 }
+
+// my subtract function
+function subtractInputs (num1, num2) {
+    let total = num1 - num2;
+    return total;
+}
+
+// my multiply function
+function multiplyInputs ()
+
+// my divide function
 
 
 
@@ -23,7 +43,26 @@ let totalResult = {
 app.post('/calculator', (req, res) => {
     console.log('this is our post request.')
 
-    // in our post we need to be able to take the 2 inputs and then have it calculate the answers
+    // taking the all inputs object and seperating the properties into individual values to use for other functions
+
+    console.log('this is the object from the client', req.body)
+    let inputOne = req.body.part1
+    let operator = req.body.operator1
+    let inputTwo = req.body.part2
+
+    // this is a check for the individual parts work properly
+    console.log('my obhect as individual parts', inputOne, operator, inputTwo)
+
+    // next i will need to create a switch statement 
+
+
+    
+
+
+    // add the total to the 
+    totalResult.total.push(total)
+
+    // in our post we need to be able to take the inputs and then have it calculate the answers
 
     res.sendStatus(201)
 })
